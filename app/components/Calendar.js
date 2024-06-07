2de9b766ff581fb1d22e9baf753b7630ca868e68
AdelBoussa <aboussenane@gmail.com>
2024-06-07 00:40:46 -0700

    mobile optimization

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import { Box, Button, Input, Flex, IconButton, Select, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Text, Center } from '@chakra-ui/react';
+import { Stack, Box, Button, Input, Flex, IconButton, Select,useMediaQuery, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Text, Center } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
 import Holidays from 'date-holidays';
 import { all } from 'axios';
-import { add, format } from 'date-fns';
+import { add, format, isBefore } from 'date-fns';
 import interactionPlugin from '@fullcalendar/interaction';
+
+
 function Calendar() {
   const [events, setEvents] = useState([]);
   const [holidays, setHolidays] = useState([]);
@@ -22,27 +24,27 @@ function Calendar() {
   const [duration, setDuration] = useState('');
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [conflictMessage, setConflictMessage] = useState('');
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setIsMobile(window.innerWidth < 768);
+  //   };
 
-    // Set initial value
-    handleResize();
+  //   // Set initial value
+  //   handleResize();
 
-    // Add event listener
-    window.addEventListener('resize', handleResize);
+  //   // Add event listener
+  //   window.addEventListener('resize', handleResize);
 
-    // Remove event listener on cleanup
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  //   // Remove event listener on cleanup
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, []);
     useEffect(() => {
       const fetchEvents = async () => {
         try {
-          const response = await fetch('https://booking-calendar-ten.vercel.app/api/get-bookings');
+          const response = await fetch('http://localhost:3000/api/get-bookings');
           const data = await response.json();
           console.log('Fetched events:', data.bookings); // Log fetched events
         if (Array.isArray(data.bookings)) {
@@ -79,22 +81,36 @@ function Calendar() {
       fetchHolidays();
     }, []);
     const checkForConflicts = async (startDateTime, endDateTime) => {
-      const apiUrl = `https://booking-calendar-ten.vercel.app/api/check-conflict?start_date=${encodeURIComponent(format(startDateTime, 'yyyy-MM-dd HH:mm:ss'))}&end_date=${encodeURIComponent(format(endDateTime, 'yyyy-MM-dd HH:mm:ss'))}`;
+      const apiUrl = `http://localhost:3000/api/check-conflict?start_date=${encodeURIComponent(format(startDateTime, 'yyyy-MM-dd HH:mm:ss'))}&end_date=${encodeURIComponent(format(endDateTime, 'yyyy-MM-dd HH:mm:ss'))}`;
+      console.log('Checking for conflicts with start date:', format(startDateTime, 'yyyy-MM-dd\'T\'HH:mm:ss.SSSX'), 'and end date:', format(endDateTime, 'yyyy-MM-dd\'T\'HH:mm:ss.SSSX'));
       try {
-        const response = await fetch(apiUrl, { method: 'GET' });
+        const response = await fetch(apiUrl, {
+          method: 'GET',
+        });
         const data = await response.json();
-        console.log('Conflict check data:', data);
-          return data.conflicts.length > 0;
-        } catch (error) {
-          console.error('Error checking conflicts:', error);
-          return false;
+        
+        if (data.conflicts && data.conflicts.length > 0) {
+          console.log('Conflicts found:', data.conflicts);
+          return true; // Conflicts exist
         }
+        
+        return false; // No conflicts
+      } catch (error) {
+        console.error('Error checking for conflicts:', error);
+        return false; // Assume no conflicts in case of error
+      }
     };
     const addNewEvent = async () => {
       const startDateTime = new Date(`${startDate}T${startTime}`);
       const durationMinutes = parseInt(duration, 10);
       const endDateTime = add(startDateTime, { minutes: durationMinutes });
       console.log('New event:', newEventName, newEventEmail, startDateTime, endDateTime, eventType);
+      
+      if (isBefore(startDateTime, new Date())) {
+        setConflictMessage('The start date and time must be in the future.');
+        console.log('Start date is in the past');
+        return;
+      }
       const hasConflicts = await checkForConflicts(startDateTime, endDateTime);
       if (hasConflicts) {
         setConflictMessage('This booking conflicts with an existing booking.');
@@ -110,7 +126,7 @@ function Calendar() {
         color: eventType === 'interview' ? 'green' : 'red',
       };
       setEvents([...events, newEvent]);
-      const apiUrl = `https://booking-calendar-ten.vercel.app/api/add-booking?name=${encodeURIComponent(newEventName)}&email=${encodeURIComponent(newEventEmail)}&description=${encodeURIComponent(eventType)}&start_date=${encodeURIComponent(format(startDateTime, 'yyyy-MM-dd HH:mm:ss'))}&end_date=${encodeURIComponent(format(endDateTime, 'yyyy-MM-dd HH:mm:ss'))}`;      fetch(apiUrl, {
+      const apiUrl = `http://localhost:3000/api/add-booking?name=${encodeURIComponent(newEventName)}&email=${encodeURIComponent(newEventEmail)}&description=${encodeURIComponent(eventType)}&start_date=${encodeURIComponent(format(startDateTime, 'yyyy-MM-dd HH:mm:ss'))}&end_date=${encodeURIComponent(format(endDateTime, 'yyyy-MM-dd HH:mm:ss'))}`;      fetch(apiUrl, {
         method: 'GET',
       })
       .then(response => response.json())
@@ -122,20 +138,24 @@ function Calendar() {
         setStartTime('');
         setDuration('');
         setEventType('');
-        onClose();
+        handleModalClose();
       })
       .catch(error => {
         console.error('Error adding booking:', error);
         // Optionally handle the error, e.g., show an error message
       });
+      const handleModalClose = () => {
+        setConflictMessage('');
+        onClose(); // Call the existing onClose function
+      };
     };
   return (
-    <Box width="100%" padding={4}>
+    <Box width="100%" height="100vh" padding={0} >
     
         
         <Center>
-        <Flex direction="column" align="center" width="100%" maxW={1100}>
-        <Flex mb={4} alignItems="center" justifyContent="flex-start" left={0}>
+        <Flex direction="column" align="center" width="100%" height="100%" maxW={1100}>
+        <Flex mb={4} alignItems="center" justifyContent="flex-start" left={0} width="100%">
         <IconButton
           icon={<AddIcon />}
           aria-label="Add event"
@@ -219,24 +239,63 @@ function Calendar() {
         </ModalContent>
       </Modal>
       
-          <Box width="100%" maxW={1100} overflow="auto">
-            <FullCalendar
-               plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                initialView={isMobile ? 'timeGridDay' : 'dayGridMonth'}
-                headerToolbar={{
-                  left: 'prev,next today',
-                  center: 'title',
-                  right: isMobile ? 'timeGridDay,timeGridWeek' : 'dayGridMonth,timeGridWeek,timeGridDay'
-                }}
-                editable={true}
-                selectable={true}
-                selectMirror={true}
-                dayMaxEvents={true}
-                weekends={true}
-                events={[...events, ...holidays]}
-                
-            />
-          </Box>
+      <Box
+          width="100%"
+          minWidth="344px"
+          height="100%"
+          flex="1"
+          overflow="hidden"
+          transform={isMobile ? 'scale(0.9)' : 'none'}
+          transformOrigin="top left"
+        >
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView={isMobile ? 'timeGridDay' : 'dayGridMonth'}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: isMobile ? '' : 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            customButtons={{
+              monthView: {
+                text: 'Month',
+                click: () => {
+                  const calendarApi = FullCalendar.Calendar.getApi();
+                  calendarApi.changeView('dayGridMonth');
+                },
+              },
+              weekView: {
+                text: 'Week',
+                click: () => {
+                  const calendarApi = FullCalendar.Calendar.getApi();
+                  calendarApi.changeView('timeGridWeek');
+                },
+              },
+              dayView: {
+                text: 'Day',
+                click: () => {
+                  const calendarApi = FullCalendar.Calendar.getApi();
+                  calendarApi.changeView('timeGridDay');
+                },
+              },
+            }}
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            weekends={true}
+            events={[...events, ...holidays]}
+            height="100%"
+            contentHeight="auto"
+          />
+          {isMobile && (
+            <Stack direction="column" spacing={2} mt={2}>
+              <Button onClick={() => FullCalendar.Calendar.getApi().changeView('dayGridMonth')}>Month</Button>
+              <Button onClick={() => FullCalendar.Calendar.getApi().changeView('timeGridWeek')}>Week</Button>
+              <Button onClick={() => FullCalendar.Calendar.getApi().changeView('timeGridDay')}>Day</Button>
+            </Stack>
+          )}
+        </Box>
           </Flex>
       </Center>
         
